bc29c9ddbd67d274a9d4a7e30135b991d7ce68ec
john <kiokoj81@gmail.com>
2022-12-22 20:45:42 +0300

    Creating own enumerable

--- mylist.rb
+++ mylist.rb
@@ -1,1 +1,20 @@
+equire_relative './enumerable'
+
+class MyList
+  include MyEnumerable
+
+  # Instance initialization
+  def initialize(*list)
+    @list = list
+  end
+
+  # yield each element in the list
+  def each
+    return to_enum unless block_given?
+
+    (0..(@list.length - 1)).each do |i|
+      yield @list[i]
+    end
+  end
+end
 
