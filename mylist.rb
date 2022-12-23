b99f77e0c6bcb7cc8830f93bbcba4f37aba6f0c1
Olivier-Kango <olivierkango@gmail.com>
2022-12-22 21:28:43 -0800

    Fix linter errors

--- mylist.rb
+++ mylist.rb
@@ -22,17 +22,17 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-p list.all? {|e| e < 5}
+p(list.all? { |e| e < 5 })
 # => true
-p list.all? {|e| e > 5}
+p(list.all? { |e| e > 5 })
 # => false
 
 # Test #any?
-p list.any? {|e| e == 2}
+p(list.any? { |e| e == 2 })
 # => true
-p list.any? {|e| e == 5}
+p(list.any? { |e| e == 5 })
 # => false
 
 # Test #filter
-p list.filter {|e| e.even?}
+p list.filter(&:even?)
 # => [2, 4]
